3d25fb021e7c03bccda427c5245af1f06c11746e
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-04-30 17:45:03 +0300

    feat: created a test user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -5,11 +5,42 @@ import User from '../models/user';
 const getUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const users = await User.find({});
-    res.status(200).json({ data: users });
+    return res.status(200).json({ data: users });
   } catch (error) {
     console.error(error);
-    next(Errors.internalError('На сервере произошла ошибка'));
+    return next(Errors.internalError('На сервере произошла ошибка'));
   }
 };
 
-export default getUsers;
+const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return next(Errors.authorizationError('Пользователь не найден'));
+    }
+    return res.status(200).json({ data: user });
+  } catch (error) {
+    console.error(error);
+    return next(Errors.internalError('На сервере произошла ошибка'));
+  }
+};
+
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, about, avatar } = req.body;
+
+  if (!name || !about || !avatar) {
+    return next(Errors.badRequestError('Не верные данные пользователя'));
+  }
+
+  try {
+    const user = await User.create({ name, about, avatar });
+    return res.status(201).json({ data: user });
+  } catch (error) {
+    console.error(error);
+    return next(Errors.internalError('На сервере произошла ошибка'));
+  }
+};
+
+export default { getUsers, getUserById, createUser };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,10 @@
-import { Router } from 'express';
+import {
+  Router,
+  NextFunction,
+  Request,
+  Response,
+} from 'express';
+import Errors from '../errors/errors';
 import userRouter from './users';
 import cardRouter from './cards';
 
@@ -6,5 +12,9 @@ const router = Router();
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+
+router.use((req: Request, res: Response, next: NextFunction) => {
+  next(Errors.notFoundError('Страница не найдена'));
+});
 
 export default router;
