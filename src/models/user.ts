3472b573ffddfced92e42e2b1f4df6d2b497ee55
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-05-04 22:40:50 +0300

    feat: validators userSchema, urlRegExp, refactor: user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,27 +1,44 @@
 import { model, Schema, Document } from 'mongoose';
+import validation from '../utils/validators';
 
 interface IUser extends Document {
   name: string,
   about: string,
-  avatar: string
+  avatar: string,
+  email: string,
+  password: string,
 }
 
 const userSchema = new Schema<IUser>({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
+    validate: validation.nameValidationUser,
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 200,
+    default: 'Исследователь',
+    validate: validation.aboutValidationUser,
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: validation.avatarValidationUser,
+  },
+  email: {
+    type: String,
+    required: [true, 'e-mail is required'],
+    unique: true,
+    validate: validation.emailValidationUser,
+  },
+  password: {
+    type: String,
+    required: [true, 'password is required'],
+    select: false,
   },
 });
 
