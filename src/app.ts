360473eb22297b3753f25b1025bb2fea8108b5e0
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-05-06 17:50:40 +0300

    feat: logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
 import express, {
   Application,
-  NextFunction,
-  Response,
   json,
 } from 'express';
 import mongoose from 'mongoose';
 import router from './routes/index';
-import { RequestCustom } from './utils/type';
 import errorsMiddleware from './middlewares/errors';
 import userControllers from './controllers/users';
 import authMiddleware from './middlewares/auth';
+import logger from './middlewares/logger';
 
 const server: string = '127.0.0.1:27017';
 const db: string = 'mestodb';
@@ -18,20 +16,12 @@ const { PORT = 3000 } = process.env;
 const app: Application = express();
 
 app.use(json());
-
-app.use((req: RequestCustom, res: Response, next: NextFunction) => {
-  req.user = {
-    _id: '644e7e7ea07bb507cdb036f7',
-  };
-
-  next();
-});
-
+app.use(logger.requestLogger);
 app.use('/signin', userControllers.loginUser);
 app.use('/signup', userControllers.createUser);
 app.use(authMiddleware);
 app.use('/', router);
-
+app.use(logger.errorLogger);
 app.use(errorsMiddleware);
 
 const connect = async () => {
