cc96a3d59e0d3a3f76dd7e671123ce62643ee262
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-05-07 11:14:37 +0300

    fixbugs: auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -8,14 +8,17 @@ interface IAuthReq extends Request {
   user?: string | JwtPayload
 }
 
-const authMiddleware = (req: IAuthReq, res: Response, next: NextFunction) => {
+// eslint-disable-next-line consistent-return
+const authMiddleware = async (req: IAuthReq, res: Response, next: NextFunction) => {
   const { authorization } = req.body;
 
   if (!authorization || !authorization.startWith('Bearer ')) {
-    throw CustomError.Unauthorized('Необходима авторизация');
+    return next(CustomError.Unauthorized('Необходима авторизация'));
   }
+
   const token = authorization.replace('Bearer ', '');
   let payload;
+
   try {
     payload = jwt.verify(token, process.env.TOKEN_ENV as string || secretKey);
   } catch (error) {
